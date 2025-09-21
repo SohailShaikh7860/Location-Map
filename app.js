5a0be69a5fdf4e371039e0cabbe8a92edc8dcfbb
SohailShaikh7860 <sohailshaikh6417@gmail.com>
2025-09-21 17:17:10 +0530

    Live-Location Tracking

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 import express from "express";
 import {Server} from "socket.io"
 import http from "http"
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-import path from "path";
-import { fileURLToPath } from "url";
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 io.on("connection",(socket)=>{
-    console.log("Socket connected");
+    socket.on("send-location",(data)=>{
+        io.emit("receive-location",{id:socket.id, ...data})
+    })
+    
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnect", socket.id);
+    })
 })
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use('/',(req,res)=>{
-    res.send("Home");
+    res.render("index");
 })
-
-app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
-
 server.listen(8000,()=>{
     console.log("the server has started on PORT: 8000");
 })
